e9f650cc65c05dbf4807f7c91359f27c700b71c1
Kayxue <s0990006@gmail.com>
2026-02-27 15:40:40 +0800

    Update readme.md

--- README.md
+++ README.md
@@ -1,5 +1,108 @@
 # EndfieldPuzzleSolver
-Puzzle solver for solving Originium Circuitry problem in Arknight: Endfield game.
+Puzzle solver for solving Originium Circuitry problem in Arknight: Endfield game. Inspired by [Myitian/OriginiumCircuitryPuzzleSolver](https://github.com/Myitian/OriginiumCircuitryPuzzleSolver)
 
 > [!NOTE]
 > If you want to download the release version, please wait until GitHub Actions workflow is finished.
+
+## Example Usage
+
+> [!NOTE]
+> These example usage inputs are from [Myitian/OriginiumCircuitryPuzzleSolver](Myitian/OriginiumCircuitryPuzzleSolver), because I have already solve most of the puzzle by hand, I can't take screenshot anymore.
+
+### Example 1
+## Example-1
+
+![Game screen of the example-1](Example-1.png)
+
+> ```
+> Grid:
+>   1 2 3 4 - column requirements
+> 1 . . . .
+> 2 . . . .
+> 3 . . . .
+> 4 . . . .
+>  \
+>   row requirements
+>
+> . - Empty
+> * - Unavailable
+> 0 - Occupied
+> Set the grid:
+> ```
+
+```
+.....
+.....
+..0..
+.....
+.....
+
+```
+
+> ```
+> Set the column requirements:
+> ```
+
+```
+3 5 5 5 3
+```
+
+> ```
+> Set the row requirements:
+> ```
+
+```
+3 5 5 5 3
+```
+
+> ```
+> Add a component:
+> ```
+
+```
+.0.
+000
+.0.
+
+```
+
+> ```
+> Add a component:
+> ```
+
+```
+.00
+00.
+0..
+
+```
+
+> ```
+> Add a component:
+> ```
+
+```
+.0.
+000
+.0.
+
+```
+
+> ```
+> Add a component:
+> ```
+
+```
+.00
+00.
+0..
+
+```
+
+> ```
+> Add a component:
+> ```
+
+```
+
+```
--- src/main.rs
+++ src/main.rs
@@ -4,43 +4,43 @@ mod components;
 
 fn main() {
     let board = vec![
-        ".....".to_owned(),
-        ".....".to_owned(),
-        ".....".to_owned(),
-        "...*.".to_owned(),
-        "....*".to_owned(),
-    ];
-    let column_input = "4 5 3 2 1".to_owned();
-    let row_input = "4 5 3 2 1".to_owned();
+            ".....".to_owned(),
+            ".....".to_owned(),
+            ".....".to_owned(),
+            "...*.".to_owned(),
+            "....*".to_owned(),
+        ];
+        let column_input = "4 5 3 2 1".to_owned();
+        let row_input = "4 5 3 2 1".to_owned();
 
-    let board_instance = Board::parse_board(&board).unwrap();
-    let parsed_column = Board::parse_column_nums(column_input, &board).unwrap();
-    let parsed_row = Board::parse_row_nums(row_input, &board).unwrap();
+        let board_instance = Board::parse_board(&board).unwrap();
+        let parsed_column = Board::parse_column_nums(column_input, &board).unwrap();
+        let parsed_row = Board::parse_row_nums(row_input, &board).unwrap();
 
-    let board = Board::new(parsed_row, parsed_column, board_instance);
+        let board = Board::new(parsed_row, parsed_column, board_instance);
 
-    let blocks = vec![
-        vec!["000".to_owned(), "0".to_owned()],
-        vec!["000".to_owned(), ".0".to_owned()],
-        vec!["000".to_owned(), ".0".to_owned()],
-        vec!["00".to_owned(), "0".to_owned()],
-    ];
+        let blocks = vec![
+            vec!["000".to_owned(), "0".to_owned()],
+            vec!["000".to_owned(), ".0".to_owned()],
+            vec!["000".to_owned(), ".0".to_owned()],
+            vec!["00".to_owned(), "0".to_owned()],
+        ];
 
-    let parsed_blocks: Vec<Block> = blocks
-        .iter()
-        .enumerate()
-        .map(|(index, e)| Block::new(('A' as u8 + index as u8) as char, e.to_vec()).unwrap())
-        .collect();
+        let parsed_blocks: Vec<Block> = blocks
+            .iter()
+            .enumerate()
+            .map(|(index, e)| Block::new(('A' as u8 + index as u8) as char, e.to_vec()).unwrap())
+            .collect();
 
-    let solver = Solver::new(parsed_blocks, board);
+        let solver = Solver::new(parsed_blocks, board);
 
-    solver.solve();
+        solver.solve();
 
-    for (i, solution) in solver.get_solution_states().iter().enumerate() {
-        println!("Solution {}", i + 1);
-        for r in solution.get_board().get_contents() {
-            println!("{}", String::from_iter(r));
+        for (i, solution) in solver.get_solution_states().iter().enumerate() {
+            println!("Solution {}", i + 1);
+            for r in solution.get_board().get_contents() {
+                println!("{}", String::from_iter(r));
+            }
+            println!("----------------");
         }
-        println!("----------------");
-    }
 }
