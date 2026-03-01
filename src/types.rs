2e7df91be63208b92c5127ea4095e59591d2765e
Kayxue <s0990006@gmail.com>
2026-03-01 23:35:23 +0800

    Decrease limit for storing SmallVec items on stack

--- src/types.rs
+++ src/types.rs
@@ -2,14 +2,14 @@ use smallvec::SmallVec;
 
 use crate::components::block::Block;
 
-pub type RequirementNums = SmallVec<[u8; 8]>;
+pub type RequirementNums = SmallVec<[u8; 7]>;
 
 pub type RequirementNumsAllColors = SmallVec<[RequirementNums; 2]>;
 
-pub type BoardContent = SmallVec<[SmallVec<[char; 8]>; 8]>;
+pub type BoardContent = SmallVec<[SmallVec<[char; 7]>; 7]>;
 
-pub type BlockContent = SmallVec<[SmallVec<[char; 8]>; 8]>;
+pub type BlockContent = SmallVec<[SmallVec<[char; 7]>; 7]>;
 
-pub type StringInputs = SmallVec<[String; 8]>;
+pub type StringInputs = SmallVec<[String; 7]>;
 
 pub type BlockVec = SmallVec<[Block; 8]>;
